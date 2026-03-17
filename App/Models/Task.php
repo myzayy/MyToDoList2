1d9e05fdb49cad520c252726de302f81ce0e73dd
myzayy <zaetsmaks06@gmail.com>
2026-03-17 21:23:30 +0100

    Implemented create(), delete() and toggleStatus() methods in Task
    Created and realized create($title, $description = "") { logic },
    Created and realized delete($id) { logic },
    Created and realized toggleStatus($id, $newStatus) { logic },
    
    They will be used as buttons in future.

--- App/Models/Task.php
+++ App/Models/Task.php
@@ -18,14 +18,50 @@ class Task
     {
         $this->db = $db;
     }
+    
     public function getAll()
     {
         $query =  "SELECT * FROM tasks";
 
-        $stmt = $this->db->prepare($query);
+        $stmt = $this->db->prepare($query); // make template from query
         $stmt->execute();
 
         return $stmt->fetchALL(PDO::FETCH_ASSOC); // FETCH_ASSOC takes only column names like keys
 
+    }
+
+    public function create($title, $description = "")
+    {
+        $query = "INSERT INTO tasks (title, description) VALUES (:title, :description)";
+        
+        $stmt = $this->db->prepare($query);
+
+        $stmt->bindParam(':title', $title); // give data
+        $stmt->bindParam(':description', $description);
+
+        return $stmt->execute(); // return true or false
+    }
+
+    public function delete($id)
+    {
+        $query = "DELETE FROM tasks WHERE id = :id";
+
+        $stmt = $this->db->prepare($query);
+
+        $stmt->bindParam(":id", $id);
+
+        return $stmt->execute();
+    }
+
+    public function toggleStatus($id, $newStatus)
+    {
+        $query = "UPDATE tasks SET is_completed = :status WHERE id = :id";
+        
+        $stmt = $this->db->prepare($query);
+
+        $stmt->bindParam(":status", $newStatus);
+        $stmt->bindParam(":id", $id);
+
+        return $stmt->execute();
     }
 }
--- public/index.php
+++ public/index.php
@@ -11,6 +11,9 @@ $db = new Database('localhost', 'root', '', 'todolist');
 $connection = $db->connect();
 
 $taskModel = new Task($connection);
+
+$taskModel->create('Learn OOP', "Learn PDO");
 $tasks = $taskModel->getAll();
 
+echo "<pre>";
 var_dump($tasks);
